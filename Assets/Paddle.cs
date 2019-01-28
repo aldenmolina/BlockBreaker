771fdd188b6f8146ad059fb4be339ea3883a8d81
Alden <aj.molina092@gmail.com>
2019-01-27 22:15:30 -0500

    Initial commit. Bouncing ball and movable paddle working.

---
+++ Assets/Paddle.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class Paddle : MonoBehaviour
+{
+    [SerializeField] float screenWidthInUnits = 16f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mousePositionInUnits = Input.mousePosition.x;
+        Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+        transform.position = paddlePosition;
+    }
+}
