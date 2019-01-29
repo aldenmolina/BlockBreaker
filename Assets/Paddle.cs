4c7d27fe0ae732681b929c44d62c7f9460087ac9
Alden <aj.molina092@gmail.com>
2019-01-29 09:22:49 -0500

    Added destroyable blocks and walls. First level is done and working

--- Assets/Paddle.cs
+++ Assets/Paddle.cs
@@ -6,7 +6,11 @@ using UnityEngine.UI;
 
 public class Paddle : MonoBehaviour
 {
+    //configuration parameters
+    [SerializeField] float minX = 1f;
+    [SerializeField] float maxX = 15f;
     [SerializeField] float screenWidthInUnits = 16f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,7 +21,8 @@ public class Paddle : MonoBehaviour
     void Update()
     {
         float mousePositionInUnits = Input.mousePosition.x;
-        Vector2 paddlePosition = new Vector2(mousePositionInUnits, transform.position.y);
+        Vector2 paddlePosition = new Vector2(transform.position.x, transform.position.y);
+        paddlePosition.x = Mathf.Clamp(mousePositionInUnits, minX, maxX);
         transform.position = paddlePosition;
     }
 }
